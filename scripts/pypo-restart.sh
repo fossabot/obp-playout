b22fd2d551e375788327afc23099f2767f0b304c
ohrstrom <ohrstrom@gmail.com>
2017-07-24 15:43:11 +0200

    compatibilit rework, liquidsoap v1.3.1

--- scripts/pypo-restart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "######################"
-echo "# restarting playout #"
-echo "######################"
-echo
-supervisorctl restart liquidsoap.pypo
-supervisorctl restart pypo
-supervisorctl status
