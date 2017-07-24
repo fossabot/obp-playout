362b4a1395063ec822e4de5bfcc4b0b1edf6241d
ohrstrom <ohrstrom@gmail.com>
2017-07-24 18:33:11 +0200

    updated requiremets and ls-library

--- pypo/start_liquidsoap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "start_liquidsoap - disabled"
-
-#./env/bin/python liquidsoap_scripts/generate_liquidsoap_cfg.py
-#sleep 2
-#cd liquidsoap_scripts
-#sudo -u pypo /usr/local/bin/liquidsoap -v ls_script.liq
-
